492e2c2ab2a861f7eb1f40aa1b3eeb4390149c73
ropkyns <ropkyns@gmail.com>
2025-06-09 17:29:37 +0200

    ex02 done maybe check to be sure

--- ex02/include/MutantStack.hpp
+++ ex02/include/MutantStack.hpp
@@ -1,18 +1,62 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MutanStack.hpp                                     :+:      :+:    :+:   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 16:23:29 by paulmart          #+#    #+#             */
-/*   Updated: 2025/06/05 16:23:57 by paulmart         ###   ########.fr       */
+/*   Updated: 2025/06/09 14:43:27 by paulmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MUTANSTACK_HPP
-# define MUSTANSTACK_HPP
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
+# include <cstdlib>
+# include <vector>
+# include <iostream>
+# include <algorithm>
+# include <ctime>
+# include <cstdlib>
+# include <unistd.h>
+# include <stack>
 
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public :
+		MutantStack(){}
+		~MutantStack(){}
+		MutantStack(const MutantStack &M)
+		{
+			*this = M;
+		}
+		MutantStack	&operator=(const MutantStack &M)
+		{
+			if (this != M)
+				return (*this);
+			return (*this);
+		}
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		iterator begin()
+		{
+			return (this->c.begin());
+		}
+		iterator end()
+		{
+			return(this->c.end());
+		}
+		const const_iterator begin() const
+		{
+			return (this->c.begin());
+		}
+		const const_iterator end() const
+		{
+			return(this->c.end());
+		}
+};
 
 #endif
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,9 +6,71 @@
 /*   By: paulmart <paulmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 16:25:50 by paulmart          #+#    #+#             */
-/*   Updated: 2025/06/05 16:26:03 by paulmart         ###   ########.fr       */
+/*   Updated: 2025/06/09 15:02:32 by paulmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/MutantStack.hpp"
+#include <list>
 
+int main()
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(42);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	std::cout << std::endl;
+
+
+	std::list<int> mlist;
+
+	mlist.push_back(5);
+	mlist.push_back(17);
+
+	std::cout << mlist.back() << std::endl;
+	mlist.pop_back();
+	std::cout << mlist.size() << std::endl;
+
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	mlist.push_back(42);
+	mlist.push_back(0);
+
+	std::list<int>::iterator itL = mlist.begin();
+	std::list<int>::iterator iteL = mlist.end();
+
+	++itL;
+	--itL;
+
+	while (itL != iteL)
+	{
+		std::cout << *itL << std::endl;
+		++itL;
+	}
+	std::list<int> l(mlist);
+	return 0;
+}
